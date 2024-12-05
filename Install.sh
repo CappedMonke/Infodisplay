a8d02114b319df8923fa41039b493af0627cf947
CappedMonke <schuergersilasdev@gmail.com>
2024-12-06 00:57:47 +0100

    Working shell script

--- Install.sh
+++ Install.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
 
-SOURCE_DIR="./Services"
-DEST_DIR="/etc/systemd/system"
+# Colors
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+NC='\033[0m' # No Color (reset)
 
 
-# Function to detect the package manager
+# Function to detect the package manager of OS
 detect_package_manager() {
-    if command -v apt >/dev/null 2>&1; then
+    if command -v apt >/dev/null ; then
         echo "apt"
-    elif command -v pacman >/dev/null 2>&1; then
+    elif command -v pacman >/dev/null ; then
         echo "pacman"
     else
         echo "unsupported package manager"
@@ -19,19 +23,15 @@ detect_package_manager() {
 
 # Function to check and install Vivaldi browser if not installed
 install_vivaldi() {
-    if ! command -v vivaldi >/dev/null 2>&1; then
+    if ! command -v vivaldi >/dev/null ; then
         echo "Vivaldi browser is not installed. Installing it now..."
         case $(detect_package_manager) in
             apt)
                 wget -qO- https://repo.vivaldi.com/archive/linux_signing_key.pub | sudo gpg --dearmor -o /usr/share/keyrings/vivaldi-browser-keyring.gpg
-                echo "deb [signed-by=/usr/share/keyrings/vivaldi-browser-keyring.gpg] https://repo.vivaldi.com/archive/deb/ stable main" | sudo tee /etc/apt/sources.list.d/vivaldi.list
                 sudo apt update
                 sudo apt install -y vivaldi-stable
                 ;;
             pacman)
-                echo "[vivaldi]" | sudo tee -a /etc/pacman.conf
-                echo "SigLevel = Optional TrustAll" | sudo tee -a /etc/pacman.conf
-                echo "Server = https://repo.vivaldi.com/arch/stable" | sudo tee -a /etc/pacman.conf
                 sudo pacman -Sy --noconfirm vivaldi
                 ;;
             *)
@@ -46,35 +46,65 @@ install_vivaldi() {
 }
 
 
-# Function to install a service with a custom message
+# Function to create a virtual environment and install requirements
+install_requirements() {
+    local req_file=$1
+    local dir_path=".venv"
+
+    if [ ! -d "$dir_path" ]; then
+        echo "Creating virtual environment..."
+        python3 -m venv "$dir_path"
+    else
+        echo "Virtual environment already exists."
+    fi
+
+    echo "Installing requirements from $req_file..."
+    source "$dir_path/bin/activate"
+    pip install --upgrade pip
+    pip install -r "$req_file"
+    deactivate
+}
+
+
+# Function to install a service
 install_service() {
     local service_name=$1
     local custom_message=$2
-    local source_file="$SOURCE_DIR/$service_name"
-    local dest_file="$DEST_DIR/$service_name"
+    local source_file="Services/$service_name"
+    local dest_file="/etc/systemd/system"
 
     if [ -e "$source_file" ]; then
-        echo "$custom_message (yes/no)"
+        echo -e "${BLUE}$custom_message (y/n)${NC}"
         read -r answer
-        if [[ "$answer" == "yes" ]]; then
-            # Check for Vivaldi installation if the service is AutostartBrowser.service
-            if [[ "$service_name" == "AutostartBrowser.service" ]]; then
-                install_vivaldi
-            fi
+        if [[ "$answer" == "y" ]]; then
+            case "$service_name" in
+                "AutostartBrowser.service")
+                    install_vivaldi
+                    ;;
+                "AutostartGestureRecognition.service")
+                    install_requirements "GestureRecognition/Requirements.txt"
+                    ;;
+                "AutostartServer.service")
+                    install_requirements "Server/Requirements.txt"
+                    ;;
+            esac
 
-            echo "Installing $service_name..."
+            echo "Copying $service_name to $dest_file..."
             sudo cp "$source_file" "$dest_file"
-            sudo systemctl daemon-reload
         fi
     else
         echo "File $service_name not found in $SOURCE_DIR. Skipping."
     fi
+    echo
 }
 
 
 install_service "AutostartBrowser.service" "Do you want to automatically start the browser and open the website at system boot? Installs Vivaldi browser if not already installed."
-install_service "AutostartGestureRecognition.service" "Do you want to automatically start the camera to recognize gestures at system boot? Will not work if no camera is connected."
-install_service "AutostartServer.service" "Do you want to automatically start the server at system boot?"
+install_service "AutostartGestureRecognition.service" "Do you want to automatically start the camera to recognize gestures at system boot? This will not work if no camera is connected."
+install_service "AutostartServer.service" "Do you want to automatically start the server at system boot? Usually you would do this for only one device in the network."
 
-
-echo "Installation process completed! To undo the installation, run Clean.sh."
+echo -e "${GREEN}Installation completed! To undo the installation, run Clean.sh. Do you want to reboot the system now for the changes to take effect? (y/n)${NC}"
+read -r answer
+if [[ "$answer" == "y" ]]; then
+    sudo reboot
+fi
